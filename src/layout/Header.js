194785fea7432fa23e2a72f190e4aa3430f2ddfd
Erik Stacy <erikslaststand@gmail.com>
2019-04-05 22:12:35 -0500

    Added the Header

---
+++ src/layout/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../css/Header.css';
+
+class Header extends Component {
+    render() {
+        return(
+            <div className="headerSection">
+                <div className="logo">
+                    <h1><span className="highlightText">Erik Stacy</span> Codes</h1>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Languages</a></li>
+                        <li><a href="">Portfolio</a></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Header
